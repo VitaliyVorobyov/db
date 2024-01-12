a21db25ca452386f51230f23e42f1a09b9b2b063
VitaliyVorobyov <123983999+VitaliyVorobyov@users.noreply.github.com>
2024-01-12 20:18:01 +0300

    bag fix

--- console.sql
+++ console.sql
@@ -1,27 +1,27 @@
-CREATE TABLE genres (
+CREATE TABLE IF NOT EXISTS genres (
   genre_id SERIAL PRIMARY KEY,
   genre_name VARCHAR(255)
 );
 
-CREATE TABLE artists (
+CREATE TABLE IF NOT EXISTS artists (
   artist_id SERIAL PRIMARY KEY,
   artist_name VARCHAR(255)
 );
 
-CREATE TABLE artists_genres (
+CREATE TABLE IF NOT EXISTS artists_genres (
   artist_id INT,
   genre_id INT,
   FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
   FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
 );
 
-CREATE TABLE albums (
+CREATE TABLE IF NOT EXISTS albums (
   album_id SERIAL PRIMARY KEY,
   album_name VARCHAR(255),
   release_year INT
 );
 
-CREATE TABLE tracks (
+CREATE TABLE IF NOT EXISTS tracks (
   track_id SERIAL PRIMARY KEY,
   track_name VARCHAR(255),
   duration INT,
@@ -29,20 +29,20 @@ CREATE TABLE tracks (
   FOREIGN KEY (album_id) REFERENCES albums(album_id)
 );
 
-CREATE TABLE compilations (
+CREATE TABLE IF NOT EXISTS compilations (
   compilation_id SERIAL PRIMARY KEY,
   compilation_name VARCHAR(255),
   release_year INT
 );
 
-CREATE TABLE compilations_tracks (
+CREATE TABLE IF NOT EXISTS compilations_tracks (
   compilation_id INT,
   track_id INT,
   FOREIGN KEY (compilation_id) REFERENCES compilations(compilation_id),
   FOREIGN KEY (track_id) REFERENCES tracks(track_id)
 );
 
-CREATE TABLE artists_albums (
+CREATE TABLE IF NOT EXISTS artists_albums (
   artist_id INT,
   album_id INT,
   FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
